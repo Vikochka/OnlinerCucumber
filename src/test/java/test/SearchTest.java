3dfd78b59606504dd44932117a6e57557bc1f1df
V.Yermakovich <erviktoria19@gmail.com>
2021-07-05 10:54:10 +0300

    Add "html:target/cucumber.html",                    "json:target/cucumber.json"

--- src/test/java/test/SearchTest.java
+++ src/test/java/test/SearchTest.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features ={"src/test/java/feature/search.feature"},
         glue = "steps",
-        plugin = {"pretty"}
+        plugin = {"pretty","html:target/cucumber.html",
+                "json:target/cucumber.json"}
 )
 public class SearchTest extends AbstractTestNGCucumberTests {
 }
