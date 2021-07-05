a9aeef75e53565cd0fb4337ee92806f8fffc1d5b
V.Yermakovich <erviktoria19@gmail.com>
2021-07-05 15:11:52 +0300

    Fixed navigationList

--- src/test/java/page/MainPage.java
+++ src/test/java/page/MainPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 
 @Getter
 public class MainPage extends BaseOnlinerPage {
-    private static String pageLocator = "[//a[@class='footer-3-links all-news-link']";
+    private static String pageLocator = "//a[@class='footer-3-links all-news-link']";
 
     public MainPage() {
         super(By.xpath(pageLocator), "Main page");
